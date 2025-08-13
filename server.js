2ed0449393e9a3ae347c0c94d324a0ca19747e92
ABHISHEK MAURYA <122637328+Abhishek21044@users.noreply.github.com>
2025-08-13 13:24:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,22 +4,20 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
+
 // Cors 
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(',')
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-}
+};
 
-// Default configuration looks like
-// {
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-//     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
-
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(express.static('public'));
+
+// ✅ Fix: Serve index.html on root path
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const connectDB = require('./config/db');
 connectDB();
@@ -33,6 +31,5 @@ app.set('view engine', 'ejs');
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
-
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
